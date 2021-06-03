80c29653e7bfd3933c3b62675fffa15fba65bc85
coffeeCat007 <hyfscript@163.com>
2021-06-03 16:07:42 +0800

    3 files modified, 1 added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="https://vuejs.org/images/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <router-link></router-link>
   </div>
 </template>
 
